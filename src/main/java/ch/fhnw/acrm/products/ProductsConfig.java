96e4109077f372ae35afbf1483590f2db9fb02a9
phgag <ph.gagneux@gmail.com>
2022-09-06 20:39:06 +0200

    Controller, repo, service etc

--- src/main/java/ch/fhnw/acrm/products/ProductsConfig.java
+++ src/main/java/ch/fhnw/acrm/products/ProductsConfig.java
@@ -7,44 +7,44 @@ import org.springframework.context.annotation.Configuration;
 
 import java.util.List;
 
-
-@Configuration
-public class ProductsConfig {
-
-    @Bean
-    CommandLineRunner commandLineRunner(ProductsRepository repository){
-        return args -> {
-            Products a = new Products(
-                        "Aflower",
-                        40,
-                        2
-            );
-
-            Products b = new Products(
-                    "Bflower",
-                    20,
-                    2.4
-            );
-
-            Products c = new Products(
-                    "Cflower",
-                    9,
-                    1.7
-            );
-
-            Products d = new Products(
-                    "Dflower",
-                    84,
-                    3.8
-            );
-
-            //Save to database
-            repository.saveAll(
-                    List.of(a,b,c,d)
-            );
-
-        };
-
-
-    }
-}
+//
+//@Configuration
+//public class ProductsConfig {
+//
+//    @Bean
+//    CommandLineRunner commandLineRunner(ProductsRepository repository){
+//        return args -> {
+//            Products a = new Products(
+//                        "Aflower",
+//                        40,
+//                        2
+//            );
+//
+//            Products b = new Products(
+//                    "Bflower",
+//                    20,
+//                    2.4
+//            );
+//
+//            Products c = new Products(
+//                    "Cflower",
+//                    9,
+//                    1.7
+//            );
+//
+//            Products d = new Products(
+//                    "Dflower",
+//                    84,
+//                    3.8
+//            );
+//
+//            //Save to database
+//            repository.saveAll(
+//                    List.of(a,b,c,d)
+//            );
+//
+//        };
+//
+//
+//    }
+//}
