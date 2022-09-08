e777065265465b6f0a8913ec5246c83345beac04
phgag <ph.gagneux@gmail.com>
2022-09-08 23:54:31 +0200

    api post response works and is turned into string sending distance in meters

--- src/main/java/ch/fhnw/acrm/orders/OrdersController.java
+++ src/main/java/ch/fhnw/acrm/orders/OrdersController.java
@@ -1,6 +1,7 @@
 package ch.fhnw.acrm.orders;
 
 
+import ch.fhnw.acrm.distancecalc.DistanceAPI;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -17,26 +18,34 @@ public class OrdersController {
 
 
     @Autowired
-    public OrdersController(OrdersService ordersService, OrdersRepository ordersRepository){
+    public OrdersController(OrdersService ordersService, OrdersRepository ordersRepository) {
         this.ordersService = ordersService;
         this.ordersRepository = ordersRepository;
     }
 
 
-
     @GetMapping
-    public String getOrdersView(Model model){
+    public String getOrdersView(Model model) {
         model.addAttribute("currentorders", ordersRepository.findById(1L).get());
 
         return "user/bootstraptest";
     }
 
+    @GetMapping(path = "costs")
+    public String getOrdersCosts(Model model) throws Exception {
+
+        long a = DistanceAPI.getData("Florastrasse40,4057Basel");
+        String str = Long.toString(a);
+        model.addAttribute("costs", str);
+        return "costs";
+    }
 
 
     @PostMapping(path = "/new")
     public void createNewOrder(@RequestBody Orders order){
         this.ordersService.addNewOrder(order);
     }
+
 
 
 
