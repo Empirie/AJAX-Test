3cb50f148f7d5625ffdab0a8f542773e26e76b7c
phgag <ph.gagneux@gmail.com>
2022-09-11 18:48:48 +0200

    saving tpcosts in repo from commandlinerunner2

--- src/main/java/ch/fhnw/acrm/transportcosts/TransportCosts.java
+++ src/main/java/ch/fhnw/acrm/transportcosts/TransportCosts.java
@@ -170,36 +170,36 @@ public class TransportCosts {
     }
 
 
-    TransportCosts transportCosts1 = new TransportCosts(1L, 58.65, 87.00,
-            114.50,
-            137.10,
-            160.80,
-            181.65,
-            201.45,
-            220.30,
-            238.35,
-            255.35,
-            271.45,
-            286.55);
-
-    TransportCosts transportCosts2 = new TransportCosts(2L,
-            67.00, 99.40, 130.85, 156.75, 183.80, 207.55, 230.25, 251.85, 272.40, 291.90, 310.25, 327.50);
-    TransportCosts transportCosts3 = new TransportCosts(
-            3L,
-            75.40,
-            111.90,
-            147.15,
-            176.35,
-            206.65,
-           233.50,
-           259.00,
-            283.35,
-            306.45,
-           328.40,
-           348.95,
-           368.40);
-
-
-    TransportCosts transportCosts4 = new TransportCosts(4L, 83.75, 124.30, 163.50, 195.90, 229.70, 259.45, 287.70, 314.85, 340.50, 364.75, 387.80, 409.40);
+//    TransportCosts transportCosts1 = new TransportCosts(1L, 58.65, 87.00,
+//            114.50,
+//            137.10,
+//            160.80,
+//            181.65,
+//            201.45,
+//            220.30,
+//            238.35,
+//            255.35,
+//            271.45,
+//            286.55);
+//
+//    TransportCosts transportCosts2 = new TransportCosts(2L,
+//            67.00, 99.40, 130.85, 156.75, 183.80, 207.55, 230.25, 251.85, 272.40, 291.90, 310.25, 327.50);
+//    TransportCosts transportCosts3 = new TransportCosts(
+//            3L,
+//            75.40,
+//            111.90,
+//            147.15,
+//            176.35,
+//            206.65,
+//           233.50,
+//           259.00,
+//            283.35,
+//            306.45,
+//           328.40,
+//           348.95,
+//           368.40);
+//
+//
+//    TransportCosts transportCosts4 = new TransportCosts(4L, 83.75, 124.30, 163.50, 195.90, 229.70, 259.45, 287.70, 314.85, 340.50, 364.75, 387.80, 409.40);
 }
 
--- src/main/java/ch/fhnw/acrm/transportcosts/TscCreator.java
+++ src/main/java/ch/fhnw/acrm/transportcosts/TscCreator.java
@@ -17,6 +17,24 @@ public class TscCreator implements CommandLineRunner {
     public void run(String... args) throws Exception {
 
 
+        TransportCosts transportCosts1 = new TransportCosts(1L, 58.65, 87.00,
+                114.50,
+                137.10,
+                160.80,
+                181.65,
+                201.45,
+                220.30,
+                238.35,
+                255.35,
+                271.45,
+                286.55);
+        transportCostsRepository.save(transportCosts1);
+
+
+        TransportCosts transportCosts2 = new TransportCosts(2L,
+                67.00, 99.40, 130.85, 156.75, 183.80, 207.55, 230.25, 251.85, 272.40, 291.90, 310.25, 327.50);
+        transportCostsRepository.save(transportCosts2);
+
         TransportCosts transportCosts3 = new TransportCosts(
                 3L,
                 75.40,
@@ -33,6 +51,11 @@ public class TscCreator implements CommandLineRunner {
                 368.40);
         transportCostsRepository.save(transportCosts3);
 
+        TransportCosts transportCosts4 = new TransportCosts(4L, 83.75, 124.30, 163.50, 195.90, 229.70, 259.45, 287.70, 314.85, 340.50, 364.75, 387.80, 409.40);
+        transportCostsRepository.save(transportCosts4);
+
+
+    }
 
 
     }
