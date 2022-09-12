8345cb3dbae62b7d835b2387957e65d10c8b3211
phgag <ph.gagneux@gmail.com>
2022-09-12 02:31:24 +0200

    web connect3

--- src/main/java/ch/fhnw/acrm/controller/IndexController.java
+++ src/main/java/ch/fhnw/acrm/controller/IndexController.java
@@ -5,13 +5,21 @@
 
 package ch.fhnw.acrm.controller;
 
+import ch.fhnw.acrm.business.service.AgentService;
+import org.aspectj.weaver.loadtime.Agent;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
 //Deactivated index controller as root, test
 @Controller
 public class IndexController {
 
+    private final AgentService agentService;
+
+    public IndexController(AgentService agentService) {
+        this.agentService = agentService;
+    }
 
     @GetMapping
     public String getIndexView(){
@@ -23,5 +31,18 @@ public class IndexController {
         return "homepage/homepage";
     }
 
+//
+//@RequestMapping(value = "homepage", method = {RequestMethod.GET,RequestMethod.POST})
+//    public String hello(RequestParam String boob, Model model){
+//        String greeting = "Hello " + agentService.getCurrentAgent().getName();
+//        return "homepage/homepage";
+//}
+
+@PostMapping(path = "/homepage")
+    public String goOrder() {
+    return "orders/list-orders-page";
+    }
 
 }
+
+
